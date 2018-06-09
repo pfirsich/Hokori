20cc026e7ab95b6735bf98c86909777cf9a1c8b9
Joel Schumacher <joelschum@gmail.com>
2018-06-09 10:28:01 +0200

    Tweak sound volumes

--- sounds.lua
+++ sounds.lua
@@ -2,22 +2,24 @@ require("libs.slam")
 
 local sounds = {}
 
-local function src(file, usage)
-    return love.audio.newSource("media/sounds/" .. file, usage or "static")
+local function src(file, volume, usage)
+    local src = love.audio.newSource("media/sounds/" .. file, usage or "static")
+    if volume then
+        src:setVolume(volume)
+    end
+    return src
 end
 
-sounds.dash = src("dash.wav")
-sounds.dash:setVolume(0.15)
+sounds.dash = src("dash.wav", 0.15)
 sounds.strike  = src("strike.wav")
 sounds.tackle = src("tackle.wav")
-sounds.strikeBlock = src("strikeBlock.wav")
+sounds.strikeBlock = src("strikeBlock.wav", 0.3)
 --sounds.strikeHit = src("strikeHit.wav")
 --sounds.tackleHit = src("tackleHit.wav")
 sounds.die = src("die.wav")
 
-sounds.music = src("music.ogg", "stream")
+sounds.music = src("music.ogg", 0.05, "stream")
 sounds.music:setLooping(true)
-sounds.music:setVolume(0.05)
 sounds.music:play()
 
 return sounds
